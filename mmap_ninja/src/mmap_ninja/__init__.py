76c77ae88f098395ca038fcb1c3fa81debeb406e
Hristo Vrigazov <hvrigazov@gmail.com>
2024-01-05 14:36:41 +0200

    Expose public symbols.

--- mmap_ninja/src/mmap_ninja/__init__.py
+++ mmap_ninja/src/mmap_ninja/__init__.py
@@ -1,5 +1,6 @@
 __version__ = "0.7.3"
 
+from .base import Wrapped
 from .numpy import (
     from_ndarray as np_from_ndarray,
     from_generator as np_from_generator,
@@ -8,3 +9,7 @@ from .numpy import (
     extend as np_extend,
     append as np_append
 )
+
+from .generic import open_existing
+from .ragged import RaggedMmap
+from .string import StringsMmap
