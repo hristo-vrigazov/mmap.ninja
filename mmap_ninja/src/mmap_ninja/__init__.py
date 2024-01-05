51251ac830412bdc37a1f7e4eede87bffd7ad00d
Hristo Vrigazov <hvrigazov@gmail.com>
2024-01-05 14:31:10 +0200

    Short API expose.

--- mmap_ninja/src/mmap_ninja/__init__.py
+++ mmap_ninja/src/mmap_ninja/__init__.py
@@ -1,1 +1,10 @@
-__version__ = "0.7.2"
+__version__ = "0.7.3"
+
+from .numpy import (
+    from_ndarray as np_from_ndarray,
+    from_generator as np_from_generator,
+    open_existing as np_open_existing,
+    extend_dir as np_extend_dir,
+    extend as np_extend,
+    append as np_append
+)
